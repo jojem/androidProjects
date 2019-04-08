e0bdd17866ba349be26368342591c9318a7af693
jojem <chaxjamila@gmail.com>
2019-04-09 01:10:26 +0300

    created method for buttonClick: makeText

--- lr4_jo_android/app/src/main/java/com/example/lr4_jo_android/MainActivity.java
+++ lr4_jo_android/app/src/main/java/com/example/lr4_jo_android/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lr4_jo_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  void onButtonClick(View v){
+        Toast.makeText(MainActivity.this, "Pressed Button1",
+                Toast.LENGTH_SHORT).show();
+
     }
 }
