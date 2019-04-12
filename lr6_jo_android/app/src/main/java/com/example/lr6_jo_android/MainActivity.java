594122c5c2bce0cbe013be46f5c4b59871a59614
jojem <chaxjamila@gmail.com>
2019-04-12 10:38:47 +0300

    lr6 examples: listView done

--- lr6_jo_android/app/src/main/java/com/example/lr6_jo_android/MainActivity.java
+++ lr6_jo_android/app/src/main/java/com/example/lr6_jo_android/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onBtnClick(findViewById(R.id.list));
 
 //        Locale locale = new Locale("ru");
 //        Configuration config = getBaseContext().getResources().getConfiguration();
