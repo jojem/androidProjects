a77e29f4a69eaea521ca3aa1ad89dcfadb6ea6bd
jojem <chaxjamila@gmail.com>
2019-04-12 10:32:20 +0300

    lr6 test examples: language and listView. failed

--- lr6_jo_android/app/src/main/java/com/example/lr6_jo_android/MainActivity.java
+++ lr6_jo_android/app/src/main/java/com/example/lr6_jo_android/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.lr6_jo_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.content.res.Configuration;
+import android.widget.ArrayAdapter;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Locale locale = new Locale("ru");
+//        Configuration config = getBaseContext().getResources().getConfiguration();
+//        config.locale = locale;
+//        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+    }
+
+    public void onBtnClick(View v){
+        String[] array = new String[]{"First point", "Second point", "Third point"};
+        ArrayAdapter<String> adapter = new ArrayAdapter(getApplicationContext(),
+                android.R.layout.simple_list_item_1, array);
+        ((ListView)findViewById(R.id.list)).setAdapter(adapter);
     }
 }
