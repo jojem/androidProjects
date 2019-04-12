28b24a2de0e92f0a706d3fe935a685ad5c808a03
jojem <chaxjamila@gmail.com>
2019-04-12 11:59:10 +0300

    lr6 examples: listView 2

--- lr6_jo_android/app/src/main/java/com/example/lr6_jo_android/MainActivity.java
+++ lr6_jo_android/app/src/main/java/com/example/lr6_jo_android/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.lr6_jo_android;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.content.res.Resources;
 import android.content.res.Configuration;
 import android.widget.ArrayAdapter;
 
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         onBtnClick(findViewById(R.id.list));
 
+
 //        Locale locale = new Locale("ru");
 //        Configuration config = getBaseContext().getResources().getConfiguration();
 //        config.locale = locale;
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnClick(View v){
-        String[] array = new String[]{"First point", "Second point", "Third point", "Fourth point"};
+        String[] array = getResources().getStringArray(R.array.stringArray);
         ArrayAdapter<String> adapter = new ArrayAdapter(getApplicationContext(),
                 android.R.layout.simple_list_item_1, array);
         ((ListView)v).setAdapter(adapter);
